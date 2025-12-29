647044b8bf0535ba6931324b66ed55629beea8c1
hirço <mehmet@siyaland-MacBook-Air.local>
2025-12-29 21:29:07 +0300

    Force Webpack build to fix Turbopack error

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
         config.externals.push('pino-pretty', 'lokijs', 'encoding');
         return config;
     },
-    experimental: {
-        turbopack: {}
-    }
 };
 
 export default nextConfig;
